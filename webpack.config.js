5dd8925b839846d6dbaee43156325993dc3ee55a
Anthony Rodriguez <v-anthonyro@zillowgroup.com>
2016-10-04 14:22:34 -0700

    Super basic beginning to adding React for the front end

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public/built');
+var APP_DIR = path.resolve(__dirname, 'src/client/components');
+
+module.exports = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+
+  module : {
+    loaders: [
+      {
+        test: /.jsx$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
