daa3db4b691d49f4db8d548acd64391691d16aca
Anthony Rodriguez <v-anthonyro@zillowgroup.com>
2016-10-04 13:28:20 -0700

    finally got a really good set of templating tools, and helpers in order to get the front-end format to be what I want it to be

--- src/client/app.js
+++ src/client/app.js
@@ -1,16 +1,22 @@
 import Express from 'express';
 import path from 'path';
-import favicon from 'serve-favicon';
 import Logger from 'morgan';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import hbs from 'express-hbs';
 
 import routes from './routes/index';
 
 const app = Express();
 
+app.engine('hbs', hbs.express4({
+  defaultLayout: path.join(__dirname, 'views/layouts/layout.hbs'),
+  partialsDir: path.join(__dirname, 'views'),
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+
+app.set('view engine', 'express-hbs');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
 
 app.use(Logger('dev'));
 app.use(bodyParser.json());
@@ -29,7 +35,7 @@ app.use((request, response, next) => {
 if (app.get('env') === 'development') {
   app.use((error, request, response, next) => {
     response.status(error.status || 500);
-    response.render('error', {
+    response.render('error.hbs', {
       message: error.message,
       error
     });
@@ -38,7 +44,7 @@ if (app.get('env') === 'development') {
 
 app.use((error, request, response, next) => {
   response.status(error.status || 500);
-  response.render('error', {
+  response.render('error.hbs', {
     message: error.message,
     error: {}
   });
--- src/client/routes/index.js
+++ src/client/routes/index.js
@@ -3,6 +3,12 @@ import Express from 'express';
 const routes = Express.Router();
 
 /* GET home page. */
-routes.get('/', (request, response, next) => (response.render('index')));
+routes.get('/', (request, response, next) => {
+  const context = {
+    title: 'Home'
+  };
+
+  return response.render('index.hbs', context);
+});
 
 export default routes;
